240cad5f5ca96954c9dc49f06ed84fbcb795fd43
arianaolson419 <arianaolson419@gmail.com>
2018-09-18 15:23:37 -0400

    Add functions to use absolute cooordinates

--- warmup_project/scripts/obstacle_avoider.py
+++ warmup_project/scripts/obstacle_avoider.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 import rospy
-from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan, PointCloud
 from geometry_msgs.msg import Twist, Vector3, Pose, Point
 from visualization_msgs.msg import Marker
 from std_msgs.msg import Bool, ColorRGBA
@@ -13,12 +14,32 @@ class ObstacleAvoider(object):
     def __init__(self):
         rospy.init_node('obstacle_avoider')
         self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        self.subscriber = rospy.Subscriber('/odom', Odometry, self.update_neato_pos)
         self.marker_publisher = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
-        rospy.Subscriber('/scan', LaserScan, self.process_scan)
+        rospy.Subscriber('/projected_stable_scan', PointCloud, self.process_scan)
+        self.position = Pose()
 
     def process_scan(self, msg):
-        pass
+        print("!!!!!!!!!!!!!!!!!")
 
+    def update_neato_pos(self, msg):
+        self.position = msg.pose.pose.position
+        print(self.position)
+
+    def calc_distances(self, points):
+        """Calculate the Euclidean distance between each point and the Neato.
+        Parameters
+        ---------
+        points: a list of Cartesion coordinates of the form [x, y, z].
+
+        Returns
+        -------
+        a list of Euclidian distances of the same size as points.
+        """
+        current_pos = [self.position.x, self.position.y, self.position.z]
+        distances = [np.linalg.norm([point - current_pos]) for point in points]
+        return distances
+        
     def run(self):
         rospy.spin()
 
