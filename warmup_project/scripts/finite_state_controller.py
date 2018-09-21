4861d246871a11c58b70b50011ae81f87ae2600e
arianaolson419 <arianaolson419@gmail.com>
2018-09-21 00:13:33 -0400

    Finish adding functionality

--- warmup_project/scripts/finite_state_controller.py
+++ warmup_project/scripts/finite_state_controller.py
@@ -16,47 +16,49 @@ OBST_AVOID = 2
 
 
 class FiniteStateController(object):
-	def __init__(self):
-		self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-		rospy.Subscriber('/obst/cmd_vel', Twist, self.obst_cmd_vel_cb)
-		rospy.Subscriber('/wall_follow/cmd_vel', Twist, self.wall_follow_cmd_vel_cb)
-		rospy.Subscriber('/emergency_stop', Bool, self.e_stop_cb)
-		self.obst_cmd_vel = Twist()
-		self.wall_follow_cmd_vel = Twist()
-		self.e_stop = False
+    def __init__(self):
+        rospy.init_node('finite_state_controller')
+        self.publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        rospy.Subscriber('/obst/cmd_vel', Twist, self.obst_cmd_vel_cb)
+        rospy.Subscriber('/wall_follow/cmd_vel', Twist, self.wall_follow_cmd_vel_cb)
+        rospy.Subscriber('/emergency_stop', Bool, self.e_stop_cb)
+        self.obst_cmd_vel = Twist()
+        self.wall_follow_cmd_vel = Twist()
+        self.e_stop = False
 
-	def obst_cmd_vel_cb(self, msg):
-		self.obst_cmd_vel = msg
-		self.choose_state()
+    def obst_cmd_vel_cb(self, msg):
+        self.obst_cmd_vel = msg
+        self.choose_state()
 
-	def wall_follow_cmd_vel_cb(self, msg):
-		self.wall_follow_cmd_vel = Twist()
-		self.choose_state()
+    def wall_follow_cmd_vel_cb(self, msg):
+        self.wall_follow_cmd_vel = Twist()
+        self.choose_state()
 
-	def e_stop_cb(self, msg):
-		if (msg.data == True):
-			self.publish_state(E_STOP)
-			self.e_stop = True
-		else:
-			self.e_stop = False
+    def e_stop_cb(self, msg):
+        if (msg.data == True):
+            self.publish_state(E_STOP)
+            self.e_stop = True
+        else:
+            self.e_stop = False
 
-	def publish_state(self, state):
-		if (state == E_STOP):
-			self.publisher.publish(Twist())
-		elif (state == WALL_FOLLOW):
-			self.publisher.publish(self.wall_follow_cmd_vel)
-		elif (state == OBST_AVOID):
-			self.publisher.publish(self.obst_cmd_vel)
+    def publish_state(self, state):
+        print(state)
+        if (state == E_STOP):
+            self.publisher.publish(Twist())
+        elif (state == WALL_FOLLOW):
+            self.publisher.publish(self.wall_follow_cmd_vel)
+        elif (state == OBST_AVOID):
+            self.publisher.publish(self.obst_cmd_vel)
 
-	def choose_state(self):
-		# Method of choosing what state to run in, probably based on whether the obstacle avoider is actively avoiding an obstacle or not
-		if not self.e_stop:
-			self.publish_state(E_STOP)
-			if (self.obst_cmd_vel.linear.x < 0.75):
-				self.publish_state(OBST_AVOID)
-			else:
-				self.publish_state(WALL_FOLLOW)
+    def choose_state(self):
+        # Method of choosing what state to run in, probably based on whether the obstacle avoider is actively avoiding an obstacle or not
+        if not self.e_stop:
+            if (self.obst_cmd_vel.linear.x < 0.5):
+                self.publish_state(OBST_AVOID)
+            else:
+                self.publish_state(WALL_FOLLOW)
+    def run(self):
+        rospy.spin()
 
-
-
-
+fsc = FiniteStateController()
+fsc.run()
